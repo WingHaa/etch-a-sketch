446cd7dd4ca27bcf14e4be6534d6dcb4e33f49c2
WingHaa <quanlm24@gmail.com>
2022-04-07 16:41:40 -0700

    Added unfinished slider UI

--- script.js
+++ script.js
@@ -1,50 +1,50 @@
-function setUpGrid() {
-  const container = document.querySelector('.container');
-  const colorPicker = document.querySelectorAll('.color-pad');
-  let mouseDown = false; 
-  createPad(16, 16);
-  const grid = document.querySelectorAll('.grid-item');
+const container = document.querySelector('.container');
+const colorPicker = document.querySelectorAll('.color-pad');
+let mouseDown = false; 
+createPad(16, 16); //make default grid 1st so we can target them later
+const grid = document.querySelectorAll('.grid-item');
 
-  function createPad (cols, rows) {
-    container.style.setProperty('--grid-columns', cols);
-    container.style.setProperty('--grid-rows', rows);
-    for (i = 0; i < (cols * rows); i++) {
-      let grid = document.createElement('div');
-      container.appendChild(grid).className = 'grid-item';
-    }
-  }
-  
-  colorPicker.forEach(element => {
-    element.addEventListener('click', getColor)
-  });
-  
-  let color;
-  
-  function getColor (e) {
-    color = e.target.id;
-  }
-  
-  grid.forEach(item => {
-    item.addEventListener('mousedown', () => {
-      mouseDown = true
-    })
-  });
-  
-  grid.forEach(item => {
-    item.addEventListener('mouseup', () => {
-      mouseDown = false
-    })
-  });
-  
-  grid.forEach(item => {
-    item.addEventListener('mousemove', paint)
-  });
-  
-  function paint(e) {
-    if (mouseDown) {
-      e.target.setAttribute('id', color);
-    }
+function createPad (cols, rows) {
+  container.style.setProperty('--grid-columns', cols);
+  container.style.setProperty('--grid-rows', rows);
+  for (i = 0; i < (cols * rows); i++) {
+    let grid = document.createElement('div');
+    container.appendChild(grid).className = 'grid-item';
   }
 }
 
-setUpGrid();
+colorPicker.forEach(element => {
+  element.addEventListener('click', getColor)
+});
+
+let color;
+
+function getColor (e) {
+  color = e.target.className;
+}
+
+grid.forEach(item => {
+  item.addEventListener('mousedown', () => {
+    mouseDown = true
+  })
+});
+
+grid.forEach(item => {
+  item.addEventListener('mouseup', () => {
+    mouseDown = false
+  })
+});
+
+grid.forEach(item => {
+  item.addEventListener('mousemove', paint)
+});
+
+function paint(e) {
+  if (mouseDown) {
+    e.target.setAttribute('class', color);
+  }
+}
+
+document.querySelector('.grid-size').oninput = function() {
+  document.querySelector('.')
+}
